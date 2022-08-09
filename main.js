e69df7586843cbc632e2c445b4a2c367d32b0ba9
Tom Lynch <twhlynch@gmail.com>
2022-08-09 20:07:08 +1000

    update

--- main.js
+++ main.js
@@ -14,11 +14,15 @@ function getPieceRelative(piece, [col, row]) {
 }
 
 $('.tile-d[data-row=7], .tile-d[data-row=6], .tile-d[data-row=5]').addClass('checker');
-$('.tile-d[data-row=2], .tile-d[data-row=1], .tile-d[data-row=0]').addClass('checker-o');
+$('.tile-d[data-row=3], .tile-d[data-row=1], .tile-d[data-row=0]').addClass('checker-o');
 
 $('.checker[data-row=6]').addClass('king');
 
-$(document).ready(function() {
+function prepareForMove() {
+    // if event liteners exist remove them
+    $('.checker').off('click');
+    $('.selected').off('click');
+
     $('.checker').click(function() {
         var piece = $(this);
         var loc = getPieceLocation(this);
@@ -145,69 +149,59 @@ $(document).ready(function() {
                 up2left.removeClass('selected');
             }
         }
-    });
-    $('.selected').click(function() {
-        var piecethis = this;
-        var piece = $(this);
-        var origin = $('.selected.checker');
-        if (!piece.hasClass('checker')) {
-            piece.addClass('checker');
-            origin.removeClass('checker');
-            if (origin.hasClass('king')) {
-                piece.addClass('king');
-                origin.removeClass('king');
-            }
-            //if jumped over a piece, remove it
-            // get piece class list
-            var classList = origin.getAttribute('class').split(/\s+/);
-            // remove all other than movement classes
-            for (var i = 0; i < classList.length; i++) {
-                if (classList[i] != 'checker' && classList[i] != 'king' && classList[i] != 'selected' && classList[i] != 'tile' && classList[i] != 'tile-d') {
-                    var movement = classList[i];
+        $('.selected').click(function() {
+            var piecethis = this;
+            var piece = $(this);
+            var origin = $('.selected.checker');
+            if (!piece.hasClass('checker')) {
+                piece.addClass('checker');
+                origin.removeClass('checker');
+                if (origin.hasClass('king')) {
+                    piece.addClass('king');
+                    origin.removeClass('king');
                 }
-            }
-            // if has a '2' string in it
-            if (movement.indexOf('2') > -1) {
-                // get the piece that was jumped over
-                if (movement == 'upright') {
+
+                if (piece.hasClass('up2right')) {
                     if (getPieceRelative(piecethis, [-1, 1]).hasClass('checker-o')) {
                         getPieceRelative(piecethis, [-1, 1]).removeClass('checker-o');
                     }
                 }
-                if (movement == 'upleft') {
+                if (piece.hasClass('up2left')) {
                     if (getPieceRelative(piecethis, [1, 1]).hasClass('checker-o')) {
                         getPieceRelative(piecethis, [1, 1]).removeClass('checker-o');
                     }
                 }
-                if (movement == 'downright') {
+                if (piece.hasClass('down2right')) {
                     if (getPieceRelative(piecethis, [-1, -1]).hasClass('checker-o')) {
                         getPieceRelative(piecethis, [-1, -1]).removeClass('checker-o');
                     }
                 }
-                if (movement == 'downleft') {
+                if (piece.hasClass('down2left')) {
                     if (getPieceRelative(piecethis, [1, -1]).hasClass('checker-o')) {
                         getPieceRelative(piecethis, [1, -1]).removeClass('checker-o');
                     }
-                }
+                }            
             }
-        }
-        $('.selected').removeClass('selected');
-        // remove all movement classes
-        $('.upright').removeClass('upright');
-        $('.upleft').removeClass('upleft');
-        $('.downright').removeClass('downright');            
-        $('.downleft').removeClass('downleft');
-        $('.up2right').removeClass('up2right');
-        $('.up2left').removeClass('up2left');
-        $('.down2right').removeClass('down2right');
-        $('.down2left').removeClass('down2left');
+            $('.selected').removeClass('selected');
+            // remove all movement classes
+            $('.upright').removeClass('upright');
+            $('.upleft').removeClass('upleft');
+            $('.downright').removeClass('downright');            
+            $('.downleft').removeClass('downleft');
+            $('.up2right').removeClass('up2right');
+            $('.up2left').removeClass('up2left');
+            $('.down2right').removeClass('down2right');
+            $('.down2left').removeClass('down2left');
+
+            prepareForMove();
+        });
     });
     // use .selected-o for opponent and .checker-o
-});
+}
+
+
 
 
 $(document).ready(function() {
-    $('.selected').click(function() {
-        console.log('selected');
-    });
+    prepareForMove();
 });
